70c7483c1b0daf8cdd4a64cdef4eba0cdf76eccd
Joshua Koch <jkoch@efcsystems.com>
2018-10-23 16:48:38 -0500

    Update FindPROJ.cmake; add version and static libs preference

--- CMakeModules/FindPROJ.cmake
+++ CMakeModules/FindPROJ.cmake
@@ -5,7 +5,7 @@
 #
 # This module defines:
 #
-# PROJ_INCLUDE_DIR, where to find geos.h, etc.
+# PROJ_INCLUDE_DIR, where to find proj_api.h, etc.
 # PROJ_LIBRARY, libraries to link against to use PROJ.
 # PROJ_FOUND, true if found, false if one of the above are not found.
 
@@ -23,11 +23,45 @@ find_path( PROJ_INCLUDE_DIR
   PATH_SUFFIXES
     proj4 )
 
+# Set PROJ_VERSION
+if( PROJ_INCLUDE_DIR )
+  set( _versionFile "${PROJ_INCLUDE_DIR}/proj_api.h" )
+  if( NOT EXISTS ${_versionFile} )
+    message( SEND_ERROR "Can't find ${_versionFile}" )
+  else()
+    file( READ "${_versionFile}" _versionContents )
+
+    string( REGEX MATCH "PJ_VERSION ([0-9])([0-9])([0-9])" _ ${_versionContents} )
+    set( PROJ_VERSION_MAJOR ${CMAKE_MATCH_1} )
+    set( PROJ_VERSION_MINOR ${CMAKE_MATCH_2} )
+    set( PROJ_VERSION_PATCH ${CMAKE_MATCH_3} )
+
+    set( PROJ_VERSION
+      "${PROJ_VERSION_MAJOR}.${PROJ_VERSION_MINOR}.${PROJ_VERSION_PATCH}" )
+  endif()
+endif()
+
+# Support preference of static libs
+set( CMAKE_FIND_LIBRARY_SUFFIXES_ORIG "${CMAKE_FIND_LIBRARY_SUFFIXES}" )
+if( PROJ_USE_STATIC_LIBS )
+  if( NOT WIN32 )
+    set( CMAKE_FIND_LIBRARY_SUFFIXES .a )
+  endif()
+else()
+  if( WIN32 )
+    set( CMAKE_FIND_LIBRARY_SUFFIXES _i.lib )
+  endif()
+endif()
+
+# If the user changed static libs preference, flush previous results
+if( NOT PROJ_USE_STATIC_LIBS STREQUAL PROJ_USE_STATIC_LIBS_LAST )
+  unset( PROJ_LIBRARY CACHE )
+endif()
+
 # Find PROJ library
 find_library( PROJ_LIBRARY
   NAMES
     proj
-    proj_i
   HINTS
     ${PROJ_DIR}/lib
   PATHS
@@ -36,10 +70,8 @@ find_library( PROJ_LIBRARY
     /opt/local/lib
     /sw/lib )
 
-# Set the PROJ_LIBRARY
-if( PROJ_LIBRARY )
-  #set( PROJ_LIBRARY ${PROJ_LIBRARY} CACHE STRING INTERNAL )
-endif()
+# Restore the original find library ordering
+set( CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_FIND_LIBRARY_SUFFIXES_ORIG}" )
 
 # Set PROJ_FOUND if variables are valid
 include( FindPackageHandleStandardArgs )
@@ -59,4 +91,12 @@ endif()
 if( NOT PROJ_FIND_QUIETLY )
   message( STATUS "PROJ_INCLUDE_DIR=${PROJ_INCLUDE_DIR}" )
   message( STATUS "PROJ_LIBRARY=${PROJ_LIBRARY}" )
+endif()
+
+# Record last used values of input variables
+if( DEFINED PROJ_USE_STATIC_LIBS )
+  set( PROJ_USE_STATIC_LIBS_LAST "${PROJ_USE_STATIC_LIBS}"
+    CACHE INTERNAL "Last used PROJ_USE_STATIC_LIBS value.")
+else()
+  unset( PROJ_USE_STATIC_LIBS_LAST CACHE )
 endif()
